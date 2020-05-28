4d659cb329bb505713f4081e0efa1c6178998669
pravinmchopade28 <66065558+pravinmchopade28@users.noreply.github.com>
2020-05-28 20:43:12 +0530

    Add files via upload

---
+++ ProductModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PravinChopadeTest.Models
+{
+    public class ProductModel
+    {
+        [Required]
+        public long ProductID { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+
+        public decimal Price { get; set; }
+        [Required]
+        public bool IsGSTApplicable { get; set; }
+        [Required]
+        public DateTime PurchaseDate { get; set; }
+        [Required]
+        public DateTime ExpireDate { get; set; }
+        [Required]
+        public string Color { get; set; }
+    }
+}
